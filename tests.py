7ecc2d9ee70f8609495a17f8b41266766db2caa0
kneeraj2000 <kneerajiiit@gmail.com>
2023-03-01 09:36:19 +0530

    adds more tests

--- tests.py
+++ tests.py
@@ -1,4 +1,10 @@
 import random
 
-def test_true():
+def test_1():
     assert random.choice([True, False]) == True
+
+def test_2():
+    assert True == True
+
+def test_3():
+    assert False == True
